6c2f037102b40d0abeaf598ace5764bccc704615
Sanskar Ranjan <105591284+just-a-penguin@users.noreply.github.com>
2022-08-13 15:07:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#   Importing Required Libraries
+import random
+
+#   Making a list of characters for the password to be made from
+listOfCharacters=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '\\', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~',"_"]
+
+#   Asking user to input the size of the password
+passSize=int(input("Please suggest the length of the Password :"))
+
+#   Making the password of the size given by the user
+passWord="".join(random.choices(listOfCharacters,k=passSize))
+
+#   Printing the password
+print("The suggested password is : \"",passWord,"\"",sep="")
